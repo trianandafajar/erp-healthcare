25bb5a25975bb74d9537f3bb6c126c8b9ce6db84
Triananda Fajar <mail.trianandafajar@gmail.com>
2026-06-09 14:13:28 +0700

    feat(auth): add role auth

--- server/api/auth/register.post.ts
+++ server/api/auth/register.post.ts
@@ -1,12 +1,25 @@
 import { serverSupabaseClient } from '#supabase/server'
 
 export default defineEventHandler(async (event) => {
-    const { email, password } = await readBody(event)
+    const { email, password, full_name, role } = await readBody(event)
+
+    if (!email || !password || !full_name) {
+        throw createError({
+            statusCode: 400,
+            message: 'Email, password, dan fullname wajib diisi',
+        })
+    }
 
     const supabase = await serverSupabaseClient(event)
     const { data, error } = await supabase.auth.signUp({
         email,
         password,
+        options: {
+            data: {
+                full_name,
+                role: role ?? 'patient',
+            },
+        },
     })
 
     if (error) {
