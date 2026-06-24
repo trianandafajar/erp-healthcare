e6de5a8b090a7f8b7ed5f7d0b1fd0980063fc094
Triananda Fajar <mail.trianandafajar@gmail.com>
2026-06-24 10:39:11 +0700

    fix(footer): update footer text and clean up formatting for consistency

--- app/components/dashboard/footer/FooterPanel.vue
+++ app/components/dashboard/footer/FooterPanel.vue
@@ -1,25 +1,23 @@
 <script setup lang="ts">
-import { shallowRef } from 'vue';
+import { shallowRef } from 'vue'
+
+const year = new Date().getFullYear()
 
 const footerLink = shallowRef([
-  {
-    title: 'About us'
-  },
-  {
-    title: 'Privacy'
-  },
-  {
-    title: 'Terms'
-  }
-]);
+  { title: 'About us' },
+  { title: 'Privacy' },
+  { title: 'Terms' }
+])
 </script>
+
 <template>
-  <v-footer class="px-0 footer">
+  <v-footer class="px-0 footer" color="containerbg">
     <v-row justify="center" no-gutters>
       <v-col cols="6">
         <p class="text-caption mb-0">
-          © All rights reserved <a href="/" class="text-primary">Privacy Policy</a>. Distributed by
-          <a href="#" target="_blank" rel="noopener noreferrer" class="text-primary"> HealthData </a>
+          © {{ year }} All rights reserved
+          <a href="/" class="text-primary">Privacy Policy</a>. Distributed by
+          <a href="#" target="_blank" rel="noopener noreferrer" class="text-primary">HealthData</a>
         </p>
       </v-col>
       <v-col class="text-right" cols="6">
