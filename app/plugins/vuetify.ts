ee3abe7079db264c2818bda42df667a6445ce285
Triananda Fajar <mail.trianandafajar@gmail.com>
2026-06-27 14:45:01 +0700

    fix(vuetify): correct primary color value in theme definition

--- app/plugins/vuetify.ts
+++ app/plugins/vuetify.ts
@@ -9,7 +9,7 @@ const light: ThemeDefinition = {
         'card-shadow': '0px 1px 4px rgba(0, 0, 0, 0.08)',
     },
     colors: {
-        primary: '#1677ff',
+        primary: '#176D37',
         secondary: '#8c8c8c',
         info: '#13c2c2',
         success: '#52c41a',
